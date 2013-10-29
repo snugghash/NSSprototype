fb8b7b4730d5037d22364b7b504c280ad6c78b50
GS4444 <gsuhas94@gmail.com>
2013-10-29 17:59:33 +0530

    Added splash screen

--- NSSManPrototype/gen/com/example/nssmanprototype/R.java
+++ NSSManPrototype/gen/com/example/nssmanprototype/R.java
@@ -21,20 +21,23 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int gy_vert_gradient=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
+        public static final int action_settings=0x7f080006;
+        public static final int imgLogo=0x7f080002;
         public static final int pager=0x7f080000;
         public static final int pager_title_strip=0x7f080001;
-        public static final int pushmessage=0x7f080004;
-        public static final int section_label=0x7f080002;
-        public static final int sendpush=0x7f080003;
+        public static final int pushmessage=0x7f080005;
+        public static final int section_label=0x7f080003;
+        public static final int sendpush=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int fragment_main_dummy=0x7f030001;
-        public static final int send_push_layout=0x7f030002;
+        public static final int activity_splash=0x7f030001;
+        public static final int fragment_main_dummy=0x7f030002;
+        public static final int send_push_layout=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- NSSVolPrototype/gen/com/example/nssvolprototype/R.java
+++ NSSVolPrototype/gen/com/example/nssvolprototype/R.java
@@ -21,21 +21,24 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int gy_vert_gradient=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
-        public static final int button1=0x7f080003;
+        public static final int action_settings=0x7f080006;
+        public static final int button1=0x7f080004;
+        public static final int imgLogo=0x7f080002;
         public static final int pager=0x7f080000;
         public static final int pager_title_strip=0x7f080001;
-        public static final int section_label=0x7f080002;
-        public static final int textView1=0x7f080004;
+        public static final int section_label=0x7f080003;
+        public static final int textView1=0x7f080005;
     }
     public static final class layout {
         public static final int activity_events_push=0x7f030000;
         public static final int activity_main=0x7f030001;
-        public static final int fragment_main_dummy=0x7f030002;
-        public static final int subscribe=0x7f030003;
+        public static final int activity_splash=0x7f030002;
+        public static final int fragment_main_dummy=0x7f030003;
+        public static final int subscribe=0x7f030004;
     }
     public static final class menu {
         public static final int events_push=0x7f070000;
