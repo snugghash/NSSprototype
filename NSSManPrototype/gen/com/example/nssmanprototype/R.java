2690d14af1b31a9cb7ec29f7ad024598722dbfa1
GS4444 <gsuhas94@gmail.com>
2013-10-29 19:00:06 +0530

    Added multiple channels for push

--- NSSManPrototype/gen/com/example/nssmanprototype/R.java
+++ NSSManPrototype/gen/com/example/nssmanprototype/R.java
@@ -22,15 +22,18 @@ public final class R {
     }
     public static final class drawable {
         public static final int gy_vert_gradient=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int gy_vert_gradient_nss=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
+        public static final int action_settings=0x7f080008;
         public static final int imgLogo=0x7f080002;
+        public static final int inst_push=0x7f080006;
         public static final int pager=0x7f080000;
         public static final int pager_title_strip=0x7f080001;
         public static final int pushmessage=0x7f080005;
         public static final int section_label=0x7f080003;
+        public static final int selectChannel=0x7f080007;
         public static final int sendpush=0x7f080004;
     }
     public static final class layout {
@@ -76,5 +79,8 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
+        /**  Splash Screen theme 
+         */
+        public static final int splashScreenTheme=0x7f060002;
     }
 }
--- NSSManPrototype/src/com/example/nssmanprototype/SendPushFragment.java
+++ NSSManPrototype/src/com/example/nssmanprototype/SendPushFragment.java
@@ -1,6 +1,7 @@
 package com.example.nssmanprototype;
 
-import com.parse.ParsePush;
+import java.util.Arrays;
+import java.util.LinkedList;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -9,7 +10,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+
+import com.parse.ParsePush;
 
 /** 
  * Fragment to display UI to send push notifications to various channels of volunteers
@@ -20,6 +22,7 @@ public class SendPushFragment extends Fragment {
 
 	EditText pushMessage;
 	Button sendPush;
+	EditText selectChannel;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -28,17 +31,21 @@ public class SendPushFragment extends Fragment {
 		super.onCreate(savedInstanceState);
 		View view = inflater.inflate(R.layout.send_push_layout, container, false);
 		pushMessage = (EditText) view.findViewById(R.id.pushmessage);
-		sendPush = (Button) view.findViewById(R.id.sendpush);	
-
+		sendPush = (Button) view.findViewById(R.id.sendpush);
+		selectChannel = (EditText) view.findViewById(R.id.selectChannel);
+		
+		//TODO Set channels
+		
 		//Send a push to Events channel with the message in EditText
 		sendPush.setOnClickListener( new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {		
 				ParsePush push = new ParsePush();
-				push.setChannel("Events");
+				LinkedList<String> channels = new LinkedList<String>(Arrays.asList(selectChannel.getText().toString().trim().split(",")));
+				push.setChannels(channels);
 				push.setMessage(pushMessage.getText().toString());
 				push.sendInBackground();
-				Toast.makeText(getActivity(), "lol", Toast.LENGTH_SHORT).show();
+				//Toast.makeText(getActivity(), "lol", Toast.LENGTH_SHORT).show();
 			}
 		});
 		
