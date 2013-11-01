23449fdecf7658c0526602c1308be5f6a7f3b502
GS4444 <gsuhas94@gmail.com>
2013-11-01 08:27:38 +0530

    Added */bin to .gitignore

--- NSSManPrototype/gen/com/example/nssmanprototype/R.java
+++ NSSManPrototype/gen/com/example/nssmanprototype/R.java
@@ -26,21 +26,30 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080008;
+        public static final int action_settings=0x7f080010;
+        public static final int editText1=0x7f080003;
+        public static final int editText2=0x7f080007;
         public static final int imgLogo=0x7f080002;
-        public static final int inst_push=0x7f080006;
+        public static final int inst_push=0x7f08000e;
         public static final int pager=0x7f080000;
         public static final int pager_title_strip=0x7f080001;
-        public static final int pushmessage=0x7f080005;
-        public static final int section_label=0x7f080003;
-        public static final int selectChannel=0x7f080007;
-        public static final int sendpush=0x7f080004;
+        public static final int pushmessage=0x7f08000d;
+        public static final int remNums=0x7f080009;
+        public static final int saveNums=0x7f080006;
+        public static final int section_label=0x7f08000b;
+        public static final int select=0x7f08000a;
+        public static final int selectChannel=0x7f08000f;
+        public static final int sendSMS=0x7f080008;
+        public static final int sendpush=0x7f08000c;
+        public static final int textView1=0x7f080004;
+        public static final int textView2=0x7f080005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_splash=0x7f030001;
-        public static final int fragment_main_dummy=0x7f030002;
-        public static final int send_push_layout=0x7f030003;
+        public static final int fragment_batchsms=0x7f030002;
+        public static final int fragment_main_dummy=0x7f030003;
+        public static final int send_push_layout=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f070000;
