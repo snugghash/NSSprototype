7209f56140a8339711c671dbf1775a9497cdf7f1
snugghash <snugghash@gmail.com>
2014-01-16 23:44:29 +0530

    Added functionality for displaying and loggin push notifications.

--- NSSVolPrototype/gen/com/example/nssvolprototype/R.java
+++ NSSVolPrototype/gen/com/example/nssvolprototype/R.java
@@ -26,20 +26,25 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
-        public static final int button1=0x7f080004;
-        public static final int imgLogo=0x7f080002;
-        public static final int pager=0x7f080000;
-        public static final int pager_title_strip=0x7f080001;
-        public static final int section_label=0x7f080003;
-        public static final int textView1=0x7f080005;
+        public static final int action_see_push_log=0x7f080001;
+        public static final int action_settings=0x7f08000a;
+        public static final int button1=0x7f080009;
+        public static final int imgLogo=0x7f080006;
+        public static final int list_message_log=0x7f080002;
+        public static final int message_log=0x7f080003;
+        public static final int pager=0x7f080004;
+        public static final int pager_title_strip=0x7f080005;
+        public static final int push_view=0x7f080000;
+        public static final int section_label=0x7f080007;
+        public static final int textView1=0x7f080008;
     }
     public static final class layout {
         public static final int activity_events_push=0x7f030000;
         public static final int activity_main=0x7f030001;
         public static final int activity_splash=0x7f030002;
         public static final int fragment_main_dummy=0x7f030003;
-        public static final int subscribe=0x7f030004;
+        public static final int log_item=0x7f030004;
+        public static final int subscribe=0x7f030005;
     }
     public static final class menu {
         public static final int events_push=0x7f070000;
--- NSSVolPrototype/src/com/example/nssvolprototype/EventsPushActivity.java
+++ NSSVolPrototype/src/com/example/nssvolprototype/EventsPushActivity.java
@@ -1,11 +1,29 @@
 package com.example.nssvolprototype;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class EventsPushActivity extends Activity {
+	
+	TextView showPush;
+	Button viewPrevNotif;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +35,82 @@ public class EventsPushActivity extends Activity {
 		//TODO Description of event
 		
 		Toast.makeText(getApplicationContext(), "You're in events!", Toast.LENGTH_SHORT).show();
+		Intent intent = getIntent();
+		JSONObject data = null;
+		String message = "Error Occured";
+		try {
+			data = new JSONObject(intent.getExtras().getString("com.parse.Data"));
+			message = (String) data.get("alert");
+		} catch (JSONException e) {
+			e.printStackTrace();
+			Log.e("debug log", "in JSONException");
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+			Log.e("debug log", "in NPE");
+		}
+		// Log.v("debug Push", message);
+		// Log.v("debug Push", data.toString());
+
+		SharedPreferences prefs = this.getApplicationContext()
+				.getSharedPreferences("org.saarang.app",
+						Context.MODE_PRIVATE);
+		
+		showPush = (TextView) findViewById(R.id.push_view);
+		//messageLog = (TextView) findViewById(R.id.message_log);
+		viewPrevNotif = (Button) findViewById(R.id.action_see_push_log);
+		viewPrevNotif.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				SharedPreferences prefs = v.getContext()
+						.getSharedPreferences("org.saarang.app",
+								Context.MODE_PRIVATE);
+
+				//messageLog.setVisibility(View.VISIBLE);
+				showPush.setVisibility(View.GONE);
+				viewPrevNotif.setVisibility(View.GONE);
+				//messageLog.setText(prefs.getString("pushlog",
+				//		"No previous logged notifications"));
+				
+				//List View to display a list of all the notifications
+				ListView listView = (ListView) findViewById(R.id.list_message_log);
+				listView.setVisibility(View.VISIBLE);
+				
+				String logString = prefs.getString("pushlog",
+						"No previous logged notifications~");
+				//Log.v("debug ergex", logString);
+				
+				String log[] = logString.split("~");
+				ArrayList<String> list = new ArrayList<String>();
+			    for (int i = 0; i < log.length; ++i) {
+			      list.add(log[i]);
+			    }
+			    ArrayAdapter<String> adapter = new ArrayAdapter<String>(v.getContext(),
+			        R.layout.log_item, list);
+			    listView.setAdapter(adapter);
+
+			}
+			
+		});
+		if (data == null || message==null) {
+			//messageLog.setVisibility(View.VISIBLE);
+			showPush.setVisibility(View.GONE);
+			//viewPrevNotif.setVisibility(View.GONE);
+			//messageLog.setText(prefs.getString("pushlog",
+			//		"No previous logged notifications"));
+		} else {
+			showPush.setText(message);
+			String pushLog = prefs.getString("pushlog","");
+			prefs.edit().putString("pushlog", pushLog + message + "~").commit();
+		}// Toast.makeText(getApplicationContext(), "You got a push with data "
+			// +
+			// data, Toast.LENGTH_SHORT).show();
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.events_push, menu);
+		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
 }
